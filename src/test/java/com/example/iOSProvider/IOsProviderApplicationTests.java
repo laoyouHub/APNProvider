086a5cab468f0248efb5c6f6301b42847789541f
xiangbo wang <laoyou_wxb@163.com>
2020-06-02 23:35:40 +0800

    APNProvider init

---
+++ src/test/java/com/example/iOSProvider/IOsProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iOSProvider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IOsProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
